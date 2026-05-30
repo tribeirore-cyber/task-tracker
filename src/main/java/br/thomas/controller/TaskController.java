f25c3e6272efdf2bf9fe2c0b8029965730c35879
Thomas <tribeiro.re@gmail.com>
2026-05-30 20:33:48 -0300

    feature: Included new endpoint to list all tasks created

--- src/main/java/br/thomas/controller/TaskController.java
+++ src/main/java/br/thomas/controller/TaskController.java
@@ -1,6 +1,9 @@
 package br.thomas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +21,11 @@ public class TaskController {
 
     @PostMapping
     public Task addTask(@RequestBody Task task) {
-        Task entity = repository.save(task);
-        return entity;
+        return repository.save(task);
+    }
+
+    @GetMapping(path = "/list")
+    public List<Task> listTasks() {
+        return repository.findAll();
     }
 }
